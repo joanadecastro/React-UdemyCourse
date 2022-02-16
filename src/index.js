f3151f641a3b116845450e217b2f2637b8b719f4
joana sousa <75800825+Joanadekastro@users.noreply.github.com>
2022-02-16 18:02:58 +0000

    FR-Primeiro componente #1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import './index.css' //aqui n criamos uma variavel ( import batatas from './index.css' pq o css nao vai ser feito neste index js...o ficheiro index.js so quer saber onde esta o index.css)
+import './index.css' //aqui n criamos uma variavel so queremos encontrar o css( import batatas from './index.css' pq o css nao vai ser feito neste index js...o ficheiro index.js so quer saber onde esta o index.css)
 import reactDom from "react-dom";//ou import batatas from 'react-dom' e em baixo escrevo a variavel batatas.render
 import React from 'react';
 
-const tag = <strong>Olá React!!</strong>;
+import Primeiro from './components/basicos/Primeiro' //no import Primeiro pode ser import batatas desde que <batatas></batatas>
+
+
 
 reactDom.render( //batatas.render
 	<div>
-		{tag}
+		<Primeiro></Primeiro>
 	</div>,
 	document.getElementById('root')
 )
