eb72a80dad7c12066af704f7bb53f016657b6845
joana sousa <75800825+Joanadekastro@users.noreply.github.com>
2022-02-16 16:11:14 +0000

    fundamentos-react- carregando CSS

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import ReactDom from "react-dom";
+import './index.css' //aqui n criamos uma variavel ( import batatas from './index.css' pq o css nao vai ser feito neste index js...o ficheiro index.js so quer saber onde esta o index.css)
+import reactDom from "react-dom";//ou import batatas from 'react-dom' e em baixo escrevo a variavel batatas.render
+import React from 'react';
 
-const el = document.getElementById('root');
+const tag = <strong>Olá React!!</strong>;
 
-ReactDom.render('Olá React!!', el)
+reactDom.render( //batatas.render
+	<div>
+		{tag}
+	</div>,
+	document.getElementById('root')
+)
